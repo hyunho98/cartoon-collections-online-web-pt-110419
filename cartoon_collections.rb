8b01b2de8425e968771a756c14545edfd7037a23
Alex Lee <hyunho98@gmail.com>
2019-11-09 23:57:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,13 @@ def summon_captain_planet(array)# code an argument here
   i << "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.each{|i|
+  if i.length > 4
+    return true
+  end}
+  false
 end
 
 def find_the_cheese# code an argument here
