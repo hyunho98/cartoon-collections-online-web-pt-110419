19bbe49888f41ac839d45e0cf24ace286cad12ee
Alex Lee <hyunho98@gmail.com>
2019-11-09 23:51:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,13 @@ def roll_call_dwarves(array)# code an argument here
   puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect{|i|
+  array[i].split
+  array[0].upcase
+  array << "!"
+  array.join}
 end
 
 def long_planeteer_calls# code an argument here
