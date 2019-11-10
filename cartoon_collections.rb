1783a139bf0a1bafc42e93e3454c16152928dcfd
Alex Lee <hyunho98@gmail.com>
2019-11-10 00:00:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,13 @@ def long_planeteer_calls(array)# code an argument here
   false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each{|i|
+    if array.include?(i)
+      return i
+    end}
+    nil
 end
